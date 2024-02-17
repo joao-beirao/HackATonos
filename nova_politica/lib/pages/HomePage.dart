0582aeb40c9ec3631f955ac8b9a8b223d7b4f038
Rui <ruiacondutoxavier@gmail.com>
2024-02-17 21:42:21 +0000

    Added firebase and register and login are working

--- nova_politica/lib/pages/HomePage.dart
+++ nova_politica/lib/pages/HomePage.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:nova_politica/pages/AboutPage.dart';
-import 'package:nova_politica/pages/QuizPage.dart';
-import 'package:url_launcher/url_launcher.dart';
-import 'ContactPage.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'GuidePage.dart';
-import 'PoliticalPartiesPage.dart';
+import 'package:nova_politica/pages/AboutPage.dart';
+import 'package:nova_politica/pages/ContactPage.dart';
+import 'package:nova_politica/pages/GuidePage.dart';
+import 'package:nova_politica/pages/PoliticalPartiesPage.dart';
+import 'package:nova_politica/pages/QuizPage.dart';
+import 'package:nova_politica/pages/login.dart';
+import 'package:nova_politica/pages/registration.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -17,19 +19,50 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool isLoggedIn = false; // Initially not logged in
+
+  void updateLoginStatus() {
+    setState(() {
+      isLoggedIn = true;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Nova Politica',
-        home: Scaffold(
-          appBar: AppBar(
-            toolbarHeight: 100,
-            title: Center(
-                child: Text('Nova Política',
-                    style: GoogleFonts.lobster(
-                        textStyle: const TextStyle(fontSize: 40)))),
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 100,
+        title: Center(child: Text('Nova Política', style: GoogleFonts.lobster(textStyle: const TextStyle(fontSize: 40)))),
+        actions: <Widget>[
+          if (!isLoggedIn) // Only show login and register buttons if not logged in
+            Row(
+              children: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => LoginScreen(onLoginSuccess: updateLoginStatus),
+                      ),
+                    );
+                  },
+                  child: const Text('Log In', style: TextStyle(color: Colors.black)),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => RegistrationScreen(),
+                      ),
+                    );
+                  },
+                  child: const Text('Register', style: TextStyle(color: Colors.black)),
+                )
+              ],
+            ),
+        ],
+      ),
           drawer: Drawer(
             child: SingleChildScrollView(
               child: Column(
@@ -101,7 +134,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       ListTile(
                         leading: const Icon(Icons.group_outlined),
                         title: const Text('Partidos'),
-                        onTap: (){
+                        onTap: () {
                           Navigator.push(
                             context,
                             MaterialPageRoute(
@@ -143,8 +176,8 @@ class _MyHomePageState extends State<MyHomePage> {
                 child: Center(
                   child: Text(
                     'Seja bem vindo ao Nova Política, uma maneira inovadora de estar a par da Política Portuguesa!',
-                    style:
-                        GoogleFonts.oswald(textStyle: const TextStyle(fontSize: 40)),
+                    style: GoogleFonts.oswald(
+                        textStyle: const TextStyle(fontSize: 40)),
                     textAlign: TextAlign.center,
                   ),
                 ),
@@ -152,8 +185,7 @@ class _MyHomePageState extends State<MyHomePage> {
               Align(
                   alignment: Alignment.topCenter,
                   child: Image.network(
-                      'https://cdn.discordapp.com/attachments/1171195359947657278/1208221458707521616/Sondagens_Iscte.png?ex=65e27eeb&is=65d009eb&hm=41603c4357c69dffcea15de2c8c8d96827097ac27d3d8469d36cd6afa302e8c6&'
-                      )),
+                      'https://cdn.discordapp.com/attachments/1171195359947657278/1208221458707521616/Sondagens_Iscte.png?ex=65e27eeb&is=65d009eb&hm=41603c4357c69dffcea15de2c8c8d96827097ac27d3d8469d36cd6afa302e8c6&')),
               Center(
                 child: Container(
                   height: 100,
@@ -175,14 +207,24 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ),
               ),
-              
-              Center(child:News1(),),
-              Container(height: 30,),
-              Center(child:News2(),),
-              Container(height: 30,),
-              Center(child:News3(),),
-              Container(height: 30,),
-
+              Center(
+                child: News1(),
+              ),
+              Container(
+                height: 30,
+              ),
+              Center(
+                child: News2(),
+              ),
+              Container(
+                height: 30,
+              ),
+              Center(
+                child: News3(),
+              ),
+              Container(
+                height: 30,
+              ),
               BottomAppBar(
                   color: Colors.grey.withOpacity(0.1),
                   child: Row(
@@ -221,110 +263,117 @@ class _MyHomePageState extends State<MyHomePage> {
                   ))
             ],
           ),
-        ));
+        );
   }
-
-
-  
 }
 
-  void _launchURL(String url) async {
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
+void _launchURL(String url) async {
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
   }
-class News1 extends StatelessWidget {  
+}
+
+class News1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-            onTap: () { // INSERIR LINK UP
-              _launchURL('https://www.dnoticias.pt/2024/2/16/394240-marcelo-recebe-ireneu-barreto-esta-manha-em-belem/');
-            },
-            child: Container( 
-      height: 270,
-      width: 680,
-      decoration: const BoxDecoration(
-        color: Color.fromARGB(255, 0, 12, 62)
-      )
-    ,
-      child: Row(children:[
-        Container(
-          padding: EdgeInsets.all(20),
-          width: 200,
-          child:Text("Marcelo recebe Ireneu Barreto esta manhã em Belém.", style: GoogleFonts.nunito(fontSize: 25, color: Color.fromARGB(255, 242, 242, 242)),textAlign: TextAlign.center),
-        ),
-        Container(
-          width: 480,
-          height: 400, 
-          child: Image.network("https://static-storage.dnoticias.pt/www-assets.dnoticias.pt/images/configuration/R/de748e4a-8d3.png"))
-
-        ])
-      ),
+      onTap: () {
+        // INSERIR LINK UP
+        _launchURL(
+            'https://www.dnoticias.pt/2024/2/16/394240-marcelo-recebe-ireneu-barreto-esta-manha-em-belem/');
+      },
+      child: Container(
+          height: 270,
+          width: 680,
+          decoration:
+              const BoxDecoration(color: Color.fromARGB(255, 0, 12, 62)),
+          child: Row(children: [
+            Container(
+              padding: EdgeInsets.all(20),
+              width: 200,
+              child: Text("Marcelo recebe Ireneu Barreto esta manhã em Belém.",
+                  style: GoogleFonts.nunito(
+                      fontSize: 25, color: Color.fromARGB(255, 242, 242, 242)),
+                  textAlign: TextAlign.center),
+            ),
+            Container(
+                width: 480,
+                height: 400,
+                child: Image.network(
+                    "https://static-storage.dnoticias.pt/www-assets.dnoticias.pt/images/configuration/R/de748e4a-8d3.png"))
+          ])),
     );
   }
 }
 
-class News2 extends StatelessWidget {  
-
+class News2 extends StatelessWidget {
   var a = "2024_02_16_21_54_34_1573604";
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-            onTap: () { // INSERIR LINK UP
-              _launchURL('https://cnnportugal.iol.pt/eleicoes-legislativas/debate/hipocrisia-lula-da-silva-ou-putin-andre-ventura-e-rui-tavares-num-debate-pouco-claro/20240216/65bd1e4ad34e65afa2fa5d50');
-            },
-      child: Container( 
-      height: 270,
-      width: 680,
-      decoration: const BoxDecoration(
-        color: Color.fromARGB(255, 0, 12, 62)
-      )
-    ,
-      child: Row(children:[
-        Container(
-          width: 480,
-          height: 400, 
-          child: Image.network("https://cdn.discordapp.com/attachments/1208282567002820609/1208282602495021067/1024.jpg?ex=65e2b7dd&is=65d042dd&hm=7dab45bb23375d511e157511f4d7cdd55945cf2a9d5cf52632fa8a0670665ad2&")),
-        Container(
-          width: 200,
-          padding: EdgeInsets.all(20),
-          child:Center(child:Text("Hipocrisia, Lula da Silva ou Putin.", style: GoogleFonts.nunito(fontSize: 25, color: Color.fromARGB(255, 242, 242, 242)),textAlign: TextAlign.center)),
-        ),
-        ])
-      ),
+      onTap: () {
+        // INSERIR LINK UP
+        _launchURL(
+            'https://cnnportugal.iol.pt/eleicoes-legislativas/debate/hipocrisia-lula-da-silva-ou-putin-andre-ventura-e-rui-tavares-num-debate-pouco-claro/20240216/65bd1e4ad34e65afa2fa5d50');
+      },
+      child: Container(
+          height: 270,
+          width: 680,
+          decoration:
+              const BoxDecoration(color: Color.fromARGB(255, 0, 12, 62)),
+          child: Row(children: [
+            Container(
+                width: 480,
+                height: 400,
+                child: Image.network(
+                    "https://cdn.discordapp.com/attachments/1208282567002820609/1208282602495021067/1024.jpg?ex=65e2b7dd&is=65d042dd&hm=7dab45bb23375d511e157511f4d7cdd55945cf2a9d5cf52632fa8a0670665ad2&")),
+            Container(
+              width: 200,
+              padding: EdgeInsets.all(20),
+              child: Center(
+                  child: Text("Hipocrisia, Lula da Silva ou Putin.",
+                      style: GoogleFonts.nunito(
+                          fontSize: 25,
+                          color: Color.fromARGB(255, 242, 242, 242)),
+                      textAlign: TextAlign.center)),
+            ),
+          ])),
     );
   }
 }
 
-class News3 extends StatelessWidget {  
+class News3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-            onTap: () { // INSERIR LINK UP
-              _launchURL('https://www.cmjornal.pt/politica/detalhe/mariana-mortagua-acusa-ps-de-falhar-na-saude-e-habitacao-pedro-nuno-contesta-solucoes-do-be-');
-            },
-            child: Container( 
-      height: 270,
-      width: 680,
-      decoration: const BoxDecoration(
-        color: Color.fromARGB(255, 0, 12, 62)
-      )
-    ,
-      child: Row(children:[
-        Container(
-          padding: EdgeInsets.all(20),
-          width: 200,
-          child:Text("Mariana Mortágua acusa PS de falhar na Saúde e Habitação.", style: GoogleFonts.nunito(fontSize: 25, color: Color.fromARGB(255, 242, 242, 242)),textAlign: TextAlign.center),
-        ),
-        Container(
-          width: 480,
-          height: 400, 
-          child: Image.network("https://cdn.discordapp.com/attachments/1208282567002820609/1208287647261397072/img_1280x7202024_02_16_21_54_34_1573604.jpeg?ex=65e2bc90&is=65d04790&hm=35c25a270e6d5e8f48188f674f764535737fcae8bad998d0bf7328993cdb8408&"))
-
-        ])
-      ),
+      onTap: () {
+        // INSERIR LINK UP
+        _launchURL(
+            'https://www.cmjornal.pt/politica/detalhe/mariana-mortagua-acusa-ps-de-falhar-na-saude-e-habitacao-pedro-nuno-contesta-solucoes-do-be-');
+      },
+      child: Container(
+          height: 270,
+          width: 680,
+          decoration:
+              const BoxDecoration(color: Color.fromARGB(255, 0, 12, 62)),
+          child: Row(children: [
+            Container(
+              padding: EdgeInsets.all(20),
+              width: 200,
+              child: Text(
+                  "Mariana Mortágua acusa PS de falhar na Saúde e Habitação.",
+                  style: GoogleFonts.nunito(
+                      fontSize: 25, color: Color.fromARGB(255, 242, 242, 242)),
+                  textAlign: TextAlign.center),
+            ),
+            Container(
+                width: 480,
+                height: 400,
+                child: Image.network(
+                    "https://cdn.discordapp.com/attachments/1208282567002820609/1208287647261397072/img_1280x7202024_02_16_21_54_34_1573604.jpeg?ex=65e2bc90&is=65d04790&hm=35c25a270e6d5e8f48188f674f764535737fcae8bad998d0bf7328993cdb8408&"))
+          ])),
     );
   }
 }
---
+++ nova_politica/lib/pages/registration.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:nova_politica/main.dart';
+import 'package:nova_politica/pages/HomePage.dart';
+import 'package:nova_politica/pages/SuccessfulRegister.dart';
+
+class RegistrationScreen extends StatelessWidget {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  RegistrationScreen({super.key});
+
+  Future<void> registerWithEmailAndPassword(BuildContext context) async {
+    try {
+      UserCredential userCredential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: emailController.text,
+        password: passwordController.text,
+      );
+      // User registered successfully, navigate to the next screen
+      // ignore: use_build_context_synchronously
+      Navigator.pushReplacementNamed(context, '/home');
+    } catch (e) {
+      // Show an error message if registration fails
+      // ignore: use_build_context_synchronously
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to register: $e')),
+      );
+    }
+  }
+
+  void validateAndRegister(BuildContext context) {
+    String email = emailController.text.trim();
+    String password = passwordController.text;
+
+    // Email validation
+    if (!isValidEmail(email)) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Invalid Email'),
+            content: Text('Please insert a valid email address.'),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+      return;
+    }
+
+    // Password validation
+    if (password.length < 6) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Invalid Password'),
+            content: Text('The password must be at least 6 characters long.'),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+      return;
+    }
+
+    // If both email and password are valid, proceed with registration
+    registerWithEmailAndPassword(context);
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => const MySuccessfulRegist(),
+      ),
+    );
+    return;
+  }
+
+  bool isValidEmail(String email) {
+    // Basic email validation using a regular expression
+    return RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(email);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Register')),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Center(
+          child: Container(
+            width: 300,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  controller: emailController,
+                  decoration: InputDecoration(labelText: 'Email'),
+                ),
+                SizedBox(height: 16), // Added spacing below email field
+                TextFormField(
+                  controller: passwordController,
+                  decoration: InputDecoration(labelText: 'Password'),
+                ),
+                SizedBox(height: 16), // Added spacing below password field
+                ElevatedButton(
+                  onPressed: () {
+                    validateAndRegister(context);
+                  },
+                  child: const Text('Register'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
