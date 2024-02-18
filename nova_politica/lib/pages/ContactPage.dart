d178b69ba51c1ded8a144b1f10a6368439972258
FrancisBoyyy <160271062+FrancisBoyyy@users.noreply.github.com>
2024-02-18 11:19:00 +0000

    adição de drawer á "sobre nós" e contactos, e formatação da "sobre nós"

--- nova_politica/lib/pages/ContactPage.dart
+++ nova_politica/lib/pages/ContactPage.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'ContactPage.dart';
+import 'AboutPage.dart';
+import 'HomePage.dart';
+import 'QuizPage.dart';
+import 'package:nova_politica/pages/ForumPage.dart';
+import 'package:nova_politica/pages/PoliticalPartiesPage.dart';
+import 'globals.dart';
 
 void main() {
   runApp(const MyContactPage());
@@ -9,8 +16,112 @@ class MyContactPage extends StatelessWidget {
 
  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+            appBar: AppBar(
+              toolbarHeight: 100,
+              //backgroundColor: Color.fromARGB(255, 255, 168, 38).withOpacity(0.7),
+              backgroundColor: Color.fromARGB(255, 255, 212, 96),
+              title: Center(
+                  child: Text('Contactos',
+                      style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold))),
+            ),
+
+          drawer: Drawer(
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Material(
+                    color: Colors.grey,
+                    child: InkWell(
+                      onTap: () {
+                        //COLOCAR AQUI O QUE FAZER QUANDO SE CLICA NA IMAGEM DA PESSOA
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.only(top: 24, bottom: 50),
+                        child: Column(
+                          children: [
+                            CircleAvatar(
+                              radius: 50,
+                              backgroundImage: NetworkImage(''),
+                            ),
+                            SizedBox(
+                              height: 12,
+                            ),
+                            Text(
+                              userEmail,
+                              style: TextStyle(fontSize: 14, color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      ListTile(
+                        leading: const Icon(Icons.home_outlined),
+                        title: const Text('Início'),
+                        onTap: () {
+                            Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (_) => const MyHomePage(title: 'Nova Política',),
+                            ),
+                          );
+                        },
+                      ),
+                      ListTile(
+                        leading: const Icon(Icons.menu_book_outlined),
+                        title: const Text('Guia para Iniciantes'),
+                        onTap: () {
+
+                        },
+                      ),
+                      ListTile(
+                        leading: const Icon(Icons.quiz_outlined),
+                        title: const Text('Quiz'),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (_) => const MyQuizApp(),
+                            ),
+                          );
+                        },
+                      ),
+                      // !!!ERRO!!! - Após iniciar o quiz, voltando para trás, já não é possível ver o menu dos partidos!
+                      ListTile(
+                        leading: const Icon(Icons.group_outlined),
+                        title: const Text('Partidos'),
+                        onTap: (){
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (_) => const MyPartiesApp(),
+                            ),
+                          );
+                        },
+                      ),
+                      ListTile(
+                        leading: const Icon(Icons.forum_outlined),
+                        title: const Text('Forum'),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (_) => ForumPage(),
+                            ),
+                          );
+                        },
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
         backgroundColor: Color(0xffffd460),
         body: SafeArea(
           child: Column(
